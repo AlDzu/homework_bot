2e22b415f607887b631a547b73ce512d26cc8121
Александр Дзюба <komrad.dzyuba2014@yandex.ru>
2022-07-01 23:49:23 +0300

    Sp7v3

--- homework.py
+++ homework.py
@@ -32,7 +32,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 1
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -56,8 +56,13 @@ def send_message(bot, message):
 
 def get_api_answer(current_timestamp):
     """Делает запрос к эндпоинту API-сервиса."""
-    # Требуется передать целочисленное значение, иначе тесты не пускают.
-    # Проверка в main()
+    # Если тип int и значение в промежутке от начала проекта до текущей даты,
+    # то похоже на правду
+    if (not type(current_timestamp) is int
+            and current_timestamp in range(1549962000, int(time.time()))):
+        message = f'Проверить дату/время {current_timestamp}'
+        logging.error(message)
+        raise TypeError(message)
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     try:
@@ -70,15 +75,12 @@ def get_api_answer(current_timestamp):
         message = f'Сбой при запросе API: {error}, {response}'
         logging.error(message)
         raise error(message)
-    else:
-        response = response.json()
-        print(response)
-        if 'error' in response or 'code' in response:
-            message = f'Неожиданный ответ API {response}'
-            logging.error(message)
-            raise RuntimeError(message)
-        else:
-            return response
+    response = response.json()
+    if 'error' in response or 'code' in response:
+        message = f'Неожиданный ответ API {response}'
+        logging.error(message)
+        raise RuntimeError(message)
+    return response
 
 
 def check_response(response):
@@ -108,18 +110,27 @@ def parse_status(homework):
 
 def check_tokens():
     """Проверяет доступность переменных окружения."""
-    if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
-        return True
-    else:
-        return False
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def main():
     """Основная логика работы бота."""
-    bot = Bot(token=TELEGRAM_TOKEN)
-    # Вот обратно затык с проверкой инициализации)
-    # Не совсем понимаю что необходимо сделать.
-    # Разве что тип содержимого проверить?
+    try:
+        bot = Bot(token=TELEGRAM_TOKEN)
+    except Exception as error:
+        message = f'Что-то не так с ботом: {error}'
+        send_message(bot, message)
+        logging.error(message)
+        raise ValueError(message)
+
+    bot_init = bot.get_me()
+    if (bot_init['id'] != '/'.join(TELEGRAM_TOKEN.split('/')[:-1])
+       and bot_init['is_bot'] is not True):
+        message = f'Что-то не так с ботом: {bot_init}'
+        send_message(bot, message)
+        logging.error(message)
+        raise ValueError(message)
+
     if (bot.id is int
         and bot.username is str
             and bot.first_name is str):
@@ -133,36 +144,25 @@ def main():
         while True:
             try:
                 current_timestamp = datetime.today() - timedelta(days=30)
-                if type(current_timestamp) is datetime:
-                    current_timestamp = int(current_timestamp.timestamp())
-                    response = get_api_answer(current_timestamp)
-                else:
-                    message = f'Проверить дату/время {current_timestamp}'
-                    logging.error(message)
-                    raise TypeError(message)
-    # Не совсем уверен что так, проверяется что datetime перед передачей
+                current_timestamp = int(current_timestamp.timestamp())
+                response = get_api_answer(current_timestamp)
             except Exception as error:
                 message = f'Сбой в работе программы: {error}'
                 send_message(bot, message)
                 logging.error(message)
-            else:
-                # относится к try
-                # "Инструкция else выполняется в том случае, если исключения не
-                # было."
-                # https://pyneng.readthedocs.io/ru/old_chapter_order/book/06_control_structures/6_exceptions.html
-                # Или так делать не рекомендуется
-                homework = check_response(response)
-                status_last_hw = homework[0]['status']  # Текущий статус работы
-                message = parse_status(homework[0])
-                if status_last_hw != status_last_hw_1:
-                    send_message(bot, message)
-                    logging.info(f'Сообщение "{message}" успешно отправлено!')
-                    status_last_hw_1 = status_last_hw
-                else:
-                    message = 'Статус работы не изменился'
-                    logging.debug(message)
             finally:
                 time.sleep(RETRY_TIME)
+
+            homework = check_response(response)
+            status_last_hw = homework[0]['status']  # Текущий статус работы
+            message = parse_status(homework[0])
+            if status_last_hw != status_last_hw_1:
+                send_message(bot, message)
+                logging.info(f'Сообщение "{message}" успешно отправлено!')
+                status_last_hw_1 = status_last_hw
+            else:
+                message = 'Статус работы не изменился'
+                logging.debug(message)
     else:
         message = 'Недостоверные значения исходных перменных!'
         send_message(bot, message)
